3a50ab664b21f553e4f510453771982311740e57
Artem BLANAR <ablanar@student.42.fr>
2020-02-04 19:14:25 +0100

    write exit on exit

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: ablanar <ablanar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 19:02:34 by ablanar           #+#    #+#             */
-/*   Updated: 2020/02/04 19:09:11 by ablanar          ###   ########.fr       */
+/*   Updated: 2020/02/04 19:13:37 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void ft_exit(char **tokens)
 {
 	free_split(tokens);
+	write(1, "exit\n", 5);
 	exit(0);
 }
