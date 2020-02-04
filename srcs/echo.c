afbfd22be0ba47ce1dce2bef724dd94ed266819d
Artem BLANAR <ablanar@e1r9p8.42.fr>
2020-02-04 13:28:25 +0100

    echo function

---
+++ srcs/echo.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablanar <ablanar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/04 12:28:49 by ablanar           #+#    #+#             */
+/*   Updated: 2020/02/04 12:54:25 by ablanar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+// Remove before flight
+#include <string.h>
+
+
+void ft_echo(char **tokens)
+{
+	int k;
+	int flag;
+
+	k = 1;
+	flag = 0;
+	if (tokens[k] != NULL && !strcmp(tokens[k], "-n"))
+	{
+		k = 2;
+		flag = 1;
+	}
+	while (tokens[k] != NULL)
+	{
+		if ((k > 1 && flag == 0) || (k > 2 && flag == 1))
+			write(1, " ", 1);
+		write(1, tokens[k], ft_strlen(tokens[k]));
+
+		k++;
+	}
+	if (!flag)
+		write(1, "\n", 1);
+}
