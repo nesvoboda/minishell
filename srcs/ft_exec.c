152d39f09f72e2d65098a4a0e186a6198361c132
Artem BLANAR <ablanar@student.42.fr>
2020-02-04 17:57:01 +0100

    Norms in ft_exec.c

--- srcs/ft_exec.c
+++ srcs/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: ablanar <ablanar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 14:57:10 by ablanar           #+#    #+#             */
-/*   Updated: 2020/02/04 17:46:26 by ablanar          ###   ########.fr       */
+/*   Updated: 2020/02/04 17:55:59 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 #include <string.h>
 #include <sys/errno.h>
 
-int ft_exec(char **tokens)
+int	ft_exec(char **tokens)
 {
-	pid_t pid;
-	pid_t wpid;
-	int status;
+	pid_t	pid;
+	pid_t	wpid;
+	int		status;
 
 	pid = fork();
 	if (pid == 0)
@@ -27,20 +27,17 @@ int ft_exec(char **tokens)
 		if (execve(tokens[0], tokens, tokens) == -1)
 		{
 			write(2, strerror(errno), ft_strlen(strerror(errno)));
-			write(2, "!\n", 2);
+			write(2, "\n", 2);
 		}
 		exit(EXIT_FAILURE);
 	}
-		else if (pid < 0)
-		{
-			write(2, "Errror in forkin\n", ft_strlen("Errror in forkin\n"));
-		}
-		else {
-	    do {
-	      wpid = waitpid(pid, &status, WUNTRACED);
-	    }
-		while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	  }
-
-	  return 1;
+	else if (pid < 0)
+		write(2, "Errror in forkin\n", ft_strlen("Errror in forkin\n"));
+	else
+	{
+		wpid = waitpid(pid, &status, WUNTRACED);
+		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+			wpid = waitpid(pid, &status, WUNTRACED);
+	}
+	return (1);
 }
