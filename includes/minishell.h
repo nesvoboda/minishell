4b3c60c74a83b5f501763c96c9555ea18c7b260f
Artem BLANAR <ablanar@student.42.fr>
2020-02-04 13:41:55 +0100

    header with echo

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 13:10:55 by ashishae          #+#    #+#             */
-/*   Updated: 2020/02/03 14:45:24 by ashishae         ###   ########.fr       */
+/*   Updated: 2020/02/04 12:41:15 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ char	*pwd(void);
 char	**ft_split(char const *s, char c);
 void	shell_loop(void);
 void	ft_putstr(char *str);
+void 	ft_echo(char **tokens);
 
 #endif
