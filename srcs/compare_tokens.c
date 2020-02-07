bf9bb19359d1f42f416aae40c9b45738f595f6fa
Arseniy SHISHAEV <ashishae@student.42.fr>
2020-02-07 20:43:59 +0100

    Separate functions to browse tokens

---
+++ srcs/compare_tokens.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compare_tokens.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/07 19:58:11 by ashishae          #+#    #+#             */
+/*   Updated: 2020/02/07 20:02:51 by ashishae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+/*
+** is() returns 1 if a token equals query, and 0 otherwise.
+*/
+
+int is(char *token, char *query)
+{
+	return (!ft_strncmp(token, query, ft_strlen(token)));
+}
+
+/*
+** is_special() returns 1 if a token is a special symbol (;/>/>>/|), and
+** 0 otherwise.
+*/
+
+int is_special(char *token)
+{
+	return (is(token, ";") || is(token, ">") || is(token, ">>") 
+				|| is(token, "|") || is(token, "<"));
+}
+
+/*
+** next_special() returns the index of the next token containing a special
+** symbol, and -1 if there is no such token in the given array.
+*/
+
+int next_special(char **tokens)
+{
+	int i;
+
+	i = 0;
+	while (tokens[i])
+	{
+		if (is_special(tokens[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
