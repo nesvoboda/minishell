68eed86c5fc820b397ef705fbcb44401fff9d7fd
Artem BLANAR <ablanar@student.42.fr>
2020-02-08 15:55:31 +0100

    ctrl-c

--- srcs/shell_loop.c
+++ srcs/shell_loop.c
@@ -6,7 +6,7 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 13:10:45 by ashishae          #+#    #+#             */
-/*   Updated: 2020/02/08 14:26:20 by ablanar          ###   ########.fr       */
+/*   Updated: 2020/02/08 15:11:42 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // Remove before flight
 #include <stdio.h>
 #include <string.h>
-
+#include <signal.h>
 /*
 ** free_split() free()s an array of strings
 */
@@ -87,24 +87,43 @@ void	free_split(char **splitted)
 // 	}
 // }
 
+
+void  INThandler(int sig)
+{
+     // char  c;
+
+     signal(sig, SIG_IGN);
+	 write(1, "\n", 1);
+	 shell_loop_2(0);
+     // printf("OUCH, did you hit Ctrl-C?\n"
+     //        "Do you really want to quit? [y/n] ");
+     // c = getchar();
+     // if (c == 'y' || c == 'Y')ma
+     //      exit(0);
+     // else
+     //      signal(SIGINT, INThandler);
+     // getchar(); // Get new line character
+}
+
 void	shell_loop_2()
 {
 	char **com;
 	int i;
 
 	i = 0;
+	signal(SIGINT, INThandler);
 	while (1)
 	{
 		write(1, "> ", 2);
 		com = ft_get_command();
 		// Diangostic output
 		// printf("kek\n");
-		while (com[i] != NULL)
-		{
-			printf("token%d %s\n",i, com[i]);
-			i++;
-		}
-		i = 0;
+		// while (com[i] != NULL)
+		// {
+		// 	printf("token%d %s\n",i, com[i]);
+		// 	i++;
+		// }
+		// i = 0;
 		// if (com[0] != NULL)
 		// 	execute(com, -1, 1);
 	}
