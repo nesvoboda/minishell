7b81a67f52736535905b47d54fbf4cc9e23b7bbb
Arseniy SHISHAEV <ashishae@student.42.fr>
2020-02-16 15:38:03 +0100

    Fix warnings when exporting unknown tokens

--- srcs/env_utils.c
+++ srcs/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 17:42:22 by ashishae          #+#    #+#             */
-/*   Updated: 2020/02/12 17:55:24 by ashishae         ###   ########.fr       */
+/*   Updated: 2020/02/16 15:37:17 by ashishae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,14 +52,16 @@ int	add_all_env(char ***our_env, char **tokens)
 {
 	int i;
 	int stop;
+	int status;
 
 	i = 1;
+	status = 0;
 	stop = next_special(tokens);
 	if (stop == -1)
 		stop = ft_tablen(tokens);
 	while (i < stop)
 	{
-		if (check_key(tokens[i]))
+		if (check_key(tokens[i]) && find_equals(tokens[i]) > 0)
 		{
 			if (find_env(*our_env, tokens[i]) < 0)
 				add_env(our_env, tokens[i]);
@@ -69,9 +71,16 @@ int	add_all_env(char ***our_env, char **tokens)
 				add_env(our_env, tokens[i]);
 			}
 		}
+		else if (check_key(tokens[i]))
+		{
+			ft_puterr("our sh: unset: `");
+			ft_puterr(tokens[i]);
+			ft_puterr("': not a valid identifier\n");
+			status = 1;
+		}
 		i++;
 	}
-	return (0);
+	return (status);
 }
 
 int	remove_all_env(char ***our_env, char **tokens)
