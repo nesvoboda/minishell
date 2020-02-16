0abda650c682ecdc8ca22132d29ca8260030e5f4
Artem BLANAR <ablanar@student.42.fr>
2020-02-16 17:23:55 +0100

    norms in check_var

--- srcs/check_var.c
+++ srcs/check_var.c
@@ -6,7 +6,7 @@
 /*   By: ablanar <ablanar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 15:28:41 by ablanar           #+#    #+#             */
-/*   Updated: 2020/02/16 17:14:10 by ablanar          ###   ########.fr       */
+/*   Updated: 2020/02/16 17:23:38 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,6 @@ char	*stringify_status(int status)
 		ret[1] = '1';
 	ret[2] = '\0';
 	return (ret);
-}
-
-char	*ft_set_env(char *key, char **our_env, t_info info)
-{
-	int		i;
-	char	*cpy;
-
-	i = 0;
-	cpy = ft_strdup(key);
-	while (cpy[i] != '\0' && cpy[i] != ' ' && cpy[i] != '\"' && cpy[i] != '$')
-		i++;
-	cpy[i] = '\0';
-	i = 0;
-	if (!ft_strncmp(cpy, "?", ft_strlen(cpy)))
-		return (stringify_status(info.status));
-	while (our_env[i])
-	{
-		if (!ft_strncmp(our_env[i], cpy, find_equals(cpy)))
-		{
-			free(cpy);
-			return (our_env[i]);
-		}
-		i++;
-	}
-	free(cpy);
-	return (NULL);
 }
 
 void	copy_var(char *new, char *tokens, char *env, int i)
