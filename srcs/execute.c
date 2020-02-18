f2261a2eb2c7f58a77569e9c0d214433eafe63b2
Artem BLANAR <ablanar@student.42.fr>
2020-02-18 21:05:17 +0100

    pwd output

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,13 +6,13 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:12:15 by ashishae          #+#    #+#             */
-/*   Updated: 2020/02/18 19:45:54 by ablanar          ###   ########.fr       */
+/*   Updated: 2020/02/18 21:05:04 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #define DEF_FD -1
-
+#include <stdio.h>
 /*
 ** execute() returns the result of the evaluation of tokens, passing an fd
 ** to any function it will launch
@@ -31,7 +31,9 @@ void	syntax_error(char *error)
 void	execute(char **tokens, int fd, int output, t_info *info)
 {
 	int special;
+	int i;
 
+	i = 0;
 	if (tokens[0] == NULL)
 		return ;
 	special = next_special(tokens);
@@ -47,6 +49,7 @@ void	execute(char **tokens, int fd, int output, t_info *info)
 		handle_redirects(tokens, fd, output, info);
 	else
 	{
+
 		switchboard(tokens, fd, output, info);
 		vpered(&tokens[special + 1], fd, output, info);
 	}
@@ -66,7 +69,7 @@ void	switchboard(char **tokens, int fd, int output, t_info *info)
 	if (is(tokens[0], "echo"))
 		info->status = ft_echo(tokens, output);
 	else if (is(tokens[0], "pwd"))
-		info->status = print_pwd(1);
+		info->status = print_pwd(output);
 	else if (is(tokens[0], "cd"))
 		info->status = ft_cd(tokens, info->our_env);
 	else if (is(tokens[0], "exit"))
