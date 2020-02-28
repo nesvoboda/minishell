26e449c007bf007f2b4d5e13392e4e9bdf04b9ce
Artem BLANAR <ablanar@student.42.fr>
2020-02-28 16:57:36 +0100

    norms

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,13 +6,13 @@
 /*   By: ablanar <ablanar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 12:28:49 by ablanar           #+#    #+#             */
-/*   Updated: 2020/02/21 20:25:48 by ablanar          ###   ########.fr       */
+/*   Updated: 2020/02/28 16:57:24 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	ft_strcmp(char *s1, char *s2)
+int		ft_strcmp(char *s1, char *s2)
 {
 	int i;
 
@@ -26,7 +26,20 @@ int	ft_strcmp(char *s1, char *s2)
 	return (0);
 }
 
-int	ft_echo(char **tokens, int fd)
+void	echo_helper(char **tokens, int fd, int flag)
+{
+	if ((!(flag)) && !is_special(tokens[0]))
+		write(fd, " ", 1);
+	if (is_special(tokens[0]))
+		return ;
+	else
+	{
+		tokens[0] = ft_copy_without_quotes(tokens[0]);
+		write(fd, tokens[0], ft_strlen(tokens[0]));
+	}
+}
+
+int		ft_echo(char **tokens, int fd)
 {
 	int k;
 	int flag;
@@ -42,15 +55,7 @@ int	ft_echo(char **tokens, int fd)
 	}
 	while (tokens[k] != NULL && !is_spec(tokens[k]))
 	{
-		if ((!(flag)) && !is_special(tokens[k]))
-			write(fd, " ", 1);
-		if (is_special(tokens[k]))
-			k++;
-		else
-		{
-			tokens[k] = ft_copy_without_quotes(tokens[k]);
-			write(fd, tokens[k], ft_strlen(tokens[k]));
-		}
+		echo_helper(&tokens[k], fd, flag);
 		flag = 0;
 		k++;
 	}
