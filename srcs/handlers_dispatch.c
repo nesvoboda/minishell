88fd5333dc5c06f03c870665186ac8d3582ab32d
Artem BLANAR <ablanar@student.42.fr>
2020-03-01 20:36:40 +0100

    left pipe

--- srcs/handlers_dispatch.c
+++ srcs/handlers_dispatch.c
@@ -6,12 +6,24 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 19:48:13 by ashishae          #+#    #+#             */
-/*   Updated: 2020/03/01 19:48:26 by ashishae         ###   ########.fr       */
+/*   Updated: 2020/03/01 20:35:26 by ablanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <../includes/minishell.h>
+#include "../includes/minishell.h"
+#include <stdio.h>
+void 	left_pipe(char **tokens, t_info *info, int tube[2], int fd)
+{
+	int io[2];
 
+	close(tube[0]);
+	info->is_forked = 1;
+	io[0] = fd;
+	io[1] = tube[1];
+	recursive_madness(tokens, io, info, tokens);
+	close(tube[1]);
+	exit(0);
+}
 void	handle_redirects(char **tokens, int fd, int output, t_info *info)
 {
 	int special;
@@ -29,13 +41,7 @@ void	handle_redirects(char **tokens, int fd, int output, t_info *info)
 		pid = fork();
 		if (pid == 0)
 		{
-			close(tube[0]);
-			info->is_forked = 1;
-			io[0] = fd;
-			io[1] = tube[1];
-			recursive_madness(tokens, io, info, tokens);
-			close(tube[1]);
-			exit(0);
+			left_pipe(tokens, info, tube, fd);
 		}
 		else
 		{
@@ -44,8 +50,6 @@ void	handle_redirects(char **tokens, int fd, int output, t_info *info)
 			{
 				close(tube[1]);
 				info->is_forked = 1;
-				// io[0] = tube[0];
-				// io[1] = output;
 				vpered(&tokens[special + 1], tube[0], output, info);
 				ft_wait_com(pid, status);
 				close(tube[0]);
@@ -55,8 +59,8 @@ void	handle_redirects(char **tokens, int fd, int output, t_info *info)
 				close(tube[0]);
 				close(tube[1]);
 				special = special + next_spec(&tokens[special + 1]) + 1;
-				printf("g_flag was %d\n", g_flag);
 				g_flag = 0;
+				printf("%d\n", special);
 				ft_wait_com(pid2, status);
 				ft_wait_com(pid, status);
 				if (special != -1 && is(tokens[special], ";"))
